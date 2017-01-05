354e7ad47cb3a084ba438e4d95653f1475668a1c
Félix Gaschi <felix.gaschi@telecom-paristech.fr>
2017-01-05 22:43:43 +0100

    resultSender.py fonctionnel

--- Modelisation/resultSender.py
+++ Modelisation/resultSender.py
@@ -6,26 +6,28 @@ import sys
 max = 50
 nbit = 8
 maxbit = 2**(nbit-1)
-coeff = max/maxbit
+coeff = 1.*max/maxbit
 
 sequence = sys.argv[1]
 
 sequence = sequence.split(";")
+print(sequence)
 
-for elt in sequence:
+instructions = [[] for elt in sequence]
+for i, elt in enumerate(sequence):
     n = int(len(elt)/8)
-    elt = [elt[j*8:(j+1)*8] for j in range(n)]
-    for elt2 in elt:
-        elt2 = coeff * (int(elt2, 2) - 2 * maxbit)
+    instructions[i] = [elt[j*8:(j+1)*8] for j in range(n)]
 
+instructions = [[(int(elt2,2) - maxbit)*coeff for elt2 in elt] for elt in instructions]
 
 with open("data", "w") as fichier:
     fichier.write("")
 
 
 with open("data", "a") as fichier:
-    for j, elt in sequence:
+    for j, elt in enumerate(instructions):
+        reInit()
         droite = [elt[2*i] for i in range(len(elt)/2)]
         gauche = [elt[(2*i)+1] for i in range(len(elt)/2)]
-        liste = [doite, gauche]
+        liste = [droite, gauche]
         fichier.write(str(stepByStep(liste))+'\n')
